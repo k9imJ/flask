be5c2b0a1901c83e2071ffc8ca930b7cb284aa2a
KimJ <ziun79@gmail.com>
2016-09-04 09:02:07 +0900

    예제 다뤄보는 파일

--- text1.py
+++ text1.py
@@ -1,11 +1,24 @@
 from flask import Flask
+from flask import render_template
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+	    return render_template('hello.html', name=name)
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-   return 'Hello World!'
+#@app.route('/')
+@app.route('/user/<username>')
+def show_user_profile(username):
+	return 'User %s' % username
+#def hello_world():
+# return 'Hello World!'
+
+	@app.route('/post/<int:post_id>')
+	def show_post(post_id):
+		return 'Post %d' % post_id
 
 if __name__ == '__main__':
 	 
-	 app.run(host='0.0.0.0')
+	 app.run()
 
